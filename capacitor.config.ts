117cf4fb3ce908d96005258a12ca85feb2d2a329
said43322-ctrl <said43322@gmail.com>
2026-09-12 20:35:53 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.masar.education",
+  appName: "مسار",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
