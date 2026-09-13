140311115c6ff558bbd2eaa4c769be9609e53e2c
said43322-ctrl <said43322@gmail.com>
2026-09-14 01:22:30 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.masar.education",
-  appName: "مسار",
+  appName: "تعلم",
   webDir: "dist",
   bundledWebRuntime: false,
 };
